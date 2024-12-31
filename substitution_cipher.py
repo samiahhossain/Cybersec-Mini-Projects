aaba03fa8d7e52a0ae97b9bf009ae101f8fc6374
samiahhossain <samiah.hoss@gmail.com>
2024-12-31 13:05:04 -0400

    substitution cipher added

---
+++ substitution_cipher.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+characters = list(" " + string.ascii_letters + string.digits + string.punctuation)
+key = characters.copy()
+random.shuffle(key)
+
+message = input("Enter your message: ")
+encoded = ''
+
+for character in message:
+    index = characters.index(character)
+    encoded += key[index]
+
+print(f"Encoded message: {encoded}")
+print("Weakness of this method: the same letter is encoded the same throughout the message, so frequency analysis and brute force can be used to help decipher.")
