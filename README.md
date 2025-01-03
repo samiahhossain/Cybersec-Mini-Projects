75df0baa4069306555d764469e25b56e4ffa14f7
samiahhossain <samiah.hoss@gmail.com>
2025-01-03 00:04:41 -0400

    email scraper comments added

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
 The following tools were made for educational purposes only. They should not be used for any unethical or illegal practices.
 
+
 WHOIS LOOKUP: Ethical hackers use this tool frequently. I followed a tutorial to run a lookup locally by using a python script and to understand what kind of information can be obtained from a lookup.
-
-Credits: W J Pearce, https://www.youtube.com/watch?v=j_XGyBzb734
-
 
 PASSWORD GENERATOR: The stronger the password, the more difficult to come up with. I wrote a script that generates a password that is 8-12 chracters long, and has at least one uppercase, one lowercase, one digit, and one special character. This ensures that the password generated meets the common password requirements.
 
-
 SUBSTITUTION CIPHER: One of the simplest encryption methods, and one I often played around with in elementary school. The script encrypts a message with a new key each time.
 
-Inspired by: Bro Code, https://www.youtube.com/watch?v=vsLBErLWBhA
-
-
 EMAIL SCRAPER: The issue of phishing emails has been around since the beginning of online communication. Learning how to build this tool showed me just how quickly websites can be searched for email addresses and personal information in general.
-
-Credits: TechAcdnt, https://www.youtube.com/watch?v=4aeqWQdMh5Q&t=326s
+ 
+Credits:
+- W J Pearce, https://www.youtube.com/watch?v=j_XGyBzb734
+- Bro Code, https://www.youtube.com/watch?v=vsLBErLWBhA
+- TechAcdnt, https://www.youtube.com/watch?v=4aeqWQdMh5Q&t=326s
--- email_scraper.py
+++ email_scraper.py
@@ -5,47 +5,60 @@ import urllib.parse
 from collections import deque
 import re
 
+# User input
 given_url = input("Enter URL: ")
 urls = deque([given_url])
+limit = int(input("How many sites do you want to search through? "))
 
+# Initializing
+count = 0
 scraped_urls = set()
 emails = set()
 
-count = 0
 try:
     while len(urls):
-        count += 1
-        if count == 100:
+        # Searching until specified limit
+        if count == limit:
             break
+        count += 1
+        
         url = urls.popleft()
         scraped_urls.add(url)
 
+        # Breaking down URL
         parts = urllib.parse.urlsplit(url)
         base_url = '{0.scheme}://{0.netloc}'.format(parts)
+        path = url[:url.rfind('/')+1] if '/' in parts.path else url     # Determine 'directory' portion of link
 
-        path = url[:url.rfind('/')+1] if '/' in parts.path else url
-
-        print('[%d] Processing %s' % (count, url))
+        # Going into site
+        print('[%d] Searching %s' % (count, url))
         try:
             response = requests.get(url)
         except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
             continue
         
+        # Collect text matching the regex (emails)
         new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
         emails.update(new_emails)
 
+        # Allow for HTML/XML parsing
         soup = BeautifulSoup(response.text, features="lxml")
 
+        # Get more links to search through from HTML/XML code
         for anchor in soup.find_all("a"):
             link = anchor.attrs['href'] if 'href' in anchor.attrs else ''
+            # Complete for full link
             if link.startswith('/'):
                 link = base_url + link
             elif not link.startswith('http'):
                 link = path + link
             if not link in urls and not link in scraped_urls:
+                # Add to URL list
                 urls.append(link)
 except KeyboardInterrupt:
     print('Closing!')
 
+# Display results
 for mail in emails:
     print(mail)
+print(f"Found {len(emails)} emails.")
